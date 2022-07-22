d01622a2de49381e8e86dd024e35509752782f93
Vladyslav Dokounin <dyacenkonikita@gmail.com>
2022-07-22 17:34:44 +0200

    update

--- Exersice.js/Exo1.js
+++ Exersice.js/Exo1.js
@@ -2,13 +2,18 @@ const myImage = document.querySelector("#myImg");
 const buttonImg = document.querySelector("#showImg");
 
 buttonImg.addEventListener("click", () => {
+  myImage.alt = "Téléchargment en cours"
   fetch(
     "https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg"
   )
-    .then((response) => response.blob())
+    .then((response) => {
+      myImage.alt = "Transformation en cours";
+      return response.blob();
+    })
     .then(function (myBlob) {
       const objectURL = URL.createObjectURL(myBlob);
+      console.log(myBlob);
+      myImage.alt = "Logo";
       myImage.src = objectURL;
     });
 });
-
--- Exersice.js/Exo2.js
+++ Exersice.js/Exo2.js
@@ -6,7 +6,7 @@ fetch(myJSON)
     console.log(studentsJSON);
     const divStudents = document.querySelector("#students");
     studentsJSON.students.forEach((student) => {
-        divStudents.innerHTML += `<div class="block  p-[22.3px] m-2 max-w-sm bg-gray rounded-lg border border-gray-400 shadow-md hover:bg-stone-300">
+        divStudents.innerHTML += `<div class="block  md:p-[22.3px] p-6 m-2 max-w-sm bg-gray rounded-lg border border-gray-400 shadow-md hover:bg-stone-300">
           <p class="mb-2 text-xl font-bold tracking-tight text-gray-900">Firstname : ${student.firstname}</p>
           <p class="mb-2 text-xl font-bold tracking-tight text-gray-900">Lastname : ${student.lastname}  </p>
           <p class="mb-2 text-xl font-bold tracking-tight text-gray-900">Githubid : ${student.githubid} </p>
